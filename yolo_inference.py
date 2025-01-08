ea295fc4f1cc8e856cfb95af222812308ca060d5
waelD1 <wael.dakkak1@gmail.com>
2025-01-08 19:39:27 +0100

    Add test for YOLO inference

---
+++ yolo_inference.py
@@ -0,0 +1,11 @@
+from ultralytics import YOLO
+
+# Load model
+model = YOLO('yolov8x')  
+
+# Apply inference
+results = model.predict('data/08fd33_4.mp4', save=True) 
+print(results[0])  # Display results
+print("\n")
+for box in results[0].boxes:
+    print(box)
